92d6807c725a17db29db1d53211e657773c020dc
Kush <kush86005@gmail.com>
2025-01-23 16:11:14 +0530

    Infinite loop using do while

---
+++ InfiniteDo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class InfiniteDo {
+    public static void main(String[] args){
+
+        do{
+            System.out.println("Hello World");
+        }while(true);
+    }
+}
