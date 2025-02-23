d80e678879adfa4edf8f0bb40e8310d149e39a06
Kush <kush86005@gmail.com>
2025-02-23 21:06:48 +0530

    Largest sum contiguous subarray(Brute force)

---
+++ Array17.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Array17{
+    public static void main(String[] args){
+
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+
+        int[] number=new int[size];
+
+        System.out.print("Enter Values:- ");
+        for(int i=0;i<size;i++){
+            number[i]=sc.nextInt();
+        }
+
+        int maxsum= Integer.MIN_VALUE;
+
+        for(int i=0;i<size;i++){
+            int sum=0;
+            for(int j=i;j<size;j++) {
+                sum += number[j];
+
+                if (maxsum < sum) {
+                    maxsum = sum;
+
+                }
+            }
+        }
+        System.out.println("The largest sum contiguous subarray:- "+maxsum);
+        }
+    }
+
