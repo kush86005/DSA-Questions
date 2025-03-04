129ff1e123bc819ab6a5fe9c31f4e4bc8a10d353
Kush <kush86005@gmail.com>
2025-03-04 18:12:33 +0530

    all pair on integer array whose sum is equal to given number

---
+++ Array30.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Array30 {
+    public static void main(String[] args){
+
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+
+        int[] number=new int[size];
+
+        System.out.print("Enter values:- ");
+        for(int i=0;i<size;i++){
+            number[i]=sc.nextInt();
+        }
+
+        System.out.print("Enter k:- ");
+        int k=sc.nextInt();
+
+        ArrayList<int[]> pair=new ArrayList<>();
+
+        for(int i=0;i<size-1;i++){
+            if(number[i]+number[i+1]==k){
+                pair.add(new int[]{number[i],number[i+1]});
+            }
+        }
+        if(pair.isEmpty()){
+            System.out.print("No pairs found");
+            return;
+        }
+        System.out.print("Pairs are:- ");
+        for(int i=0;i<pair.size();i++){
+            System.out.print(Arrays.toString(pair.get(i))+" ");
+        }
+    }
+}
