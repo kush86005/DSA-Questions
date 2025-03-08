a08389233c4e698764b245a776121cf94d5703d6
Kush <kush86005@gmail.com>
2025-03-08 22:36:50 +0530

    Maximum profit by buying and selling a stock atmost twice

---
+++ Array38.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Array38 {
+    public static void main(String[] args){
+
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+
+        int[] num=new int[size];
+
+        System.out.print("Enter values:- ");
+        for(int i=0;i<size;i++){
+            num[i]=sc.nextInt();
+        }
+
+        int profit=0;
+
+        for(int i=0;i<size-1;i++){
+            if(num[i]<num[i+1]){
+                profit +=num[i+1]-num[i];
+            }
+        }
+        System.out.print("The maximum profit will be:- "+profit);
+    }
+}
