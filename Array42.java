1277577bd754cd8e235ed4123bd690bbc767771e
Kush <kush86005@gmail.com>
2025-03-11 10:13:11 +0530

    Choclate distribution Problem

---
+++ Array42.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Array42 {
+    public static void main(String[] args){
+
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+
+        if(size<3){
+            System.out.print("Invalid Output");
+            return;
+        }
+        int num[]=new int[size];
+
+        System.out.print("Enter values:- ");
+        for(int i=0;i<size;i++){
+            num[i]=sc.nextInt();
+        }
+
+        System.out.print("Enter k:- ");
+        int k=sc.nextInt();
+
+        Arrays.sort(num);
+
+        int minDiff=Integer.MAX_VALUE;
+
+        for(int i=0;i<size-k;i++){
+            int diff=num[i+k-1]-num[i];
+
+            minDiff=Math.min(minDiff,diff);
+        }
+
+        System.out.print("Minimum difference is:- "+minDiff);
+    }
+}
