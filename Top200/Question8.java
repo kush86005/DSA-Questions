05ae48b0137acc3f32d1a08d13d6ea8a241a6283
kush <kush86005@gmail.com>
2025-08-08 18:31:26 +0530

    Print repeating pattern for n

---
+++ Top200/Question8.java
@@ -0,0 +1,18 @@
+package Top200;
+
+import java.util.*;
+public class Question8 {
+    public static void main(String[] args){
+
+        System.out.print("Enter n:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            System.out.println();
+            for(int j=0;j<n;j++){
+                System.out.print(n);
+            }
+        }
+    }
+}
