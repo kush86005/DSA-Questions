831c648444b11d6273ca7027239d00e4bb82a356
kush <kush86005@gmail.com>
2025-08-22 00:09:11 +0530

    Best Approach

--- DSASheet/Temp.java
+++ DSASheet/Temp.java
@@ -2,21 +2,17 @@ package DSASheet;
 import java.util.*;
 public class Temp {
    public static boolean Anagram(String s,String t) {
-      HashMap<Character,Integer> digit=new HashMap<>();
       int n=s.length();
       int q=t.length();
       if(n!=q) return false;
+      int[] result=new int[26];
       for(int i=0;i<n;i++){
-         char c=s.charAt(i);
-         digit.put(c,digit.getOrDefault(c,0)+1);
+         result[s.charAt(i)-'a']++;
+         result[t.charAt(i)-'a']--;
       }
-      for(int i=0;i<q;i++){
-         char c=t.charAt(i);
-         if(!digit.containsKey(c)||digit.get(c)==0){
+      for(int num:result){
+         if(num!=0){
             return false;
-         }
-         if(digit.containsKey(c)){
-            digit.put(c,digit.get(c)-1);
          }
       }
       return true;
