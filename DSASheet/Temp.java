cecdbdb0b47a2cf8c070378028dc8a82a3bc35b4
kush <kush86005@gmail.com>
2025-08-21 21:12:40 +0530

    2sum

--- DSASheet/Temp.java
+++ DSASheet/Temp.java
@@ -1,37 +1,33 @@
 package DSASheet;
 import java.util.*;
 public class Temp {
-    public static int[] twosum(int[] arr,int target){
-        HashMap<Integer,Integer> digit=new HashMap<>();
-        int n=arr.length;
-        int[] result=new int[2];
-        for(int i=0;i<n;i++){
-            int tar=target-arr[i];
+   public static int sorted(int[] arr,int target){
+      HashMap<Integer,Integer> digit=new HashMap<>();
+      int count=0;
+      int n=arr.length;
+      for(int i=0;i<n;i++){
+         int tar=target-arr[i];
 
-            if(digit.containsKey(tar)){
-                result[0]=i;
-                result[1]=digit.get(tar);
-                break;
-            }
-            digit.put(arr[i],i);
-        }
-        return result;
-    }
-    public static void main(String[] args){
-        System.out.print("Enter size:- ");
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        System.out.print("Enter elements- ");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        System.out.print("Enter target:- ");
-        int target=sc.nextInt();
+         if(digit.containsKey(tar)){
+            count+=digit.get(tar);
+         }
+         digit.put(arr[i],digit.getOrDefault(arr[i],0)+1);
+      }
+      return count;
+   }
+   public static void main(String[] args){
+      System.out.print("Enter size:- ");
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int[] arr=new int[n];
+      System.out.print("Enter elements:- ");
+      for(int i=0;i<n;i++){
+         arr[i]=sc.nextInt();
+      }
+      System.out.print("Enter target:- ");
+      int target=sc.nextInt();
 
-        int[] result=twosum(arr,target);
-        for(int i=0;i<result.length;i++){
-            System.out.print(result[i]+" ");
-        }
-    }
+      int ans=sorted(arr,target);
+      System.out.print(ans);
+   }
 }
