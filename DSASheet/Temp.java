c4386ee52b668cc95e7f617d7e5eda16d04ea3c6
kush <kush86005@gmail.com>
2025-08-22 00:31:11 +0530

    Group Anagram

--- DSASheet/Temp.java
+++ DSASheet/Temp.java
@@ -1,30 +1,36 @@
 package DSASheet;
 import java.util.*;
 public class Temp {
-   public static boolean Anagram(String s,String t) {
-      int n=s.length();
-      int q=t.length();
-      if(n!=q) return false;
-      int[] result=new int[26];
+   public static List<List<String>> groupAnagrams(String[] strs) {
+      HashMap<String,List<String>> digit=new HashMap<>();
+      int n=strs.length;
       for(int i=0;i<n;i++){
-         result[s.charAt(i)-'a']++;
-         result[t.charAt(i)-'a']--;
-      }
-      for(int num:result){
-         if(num!=0){
-            return false;
+         char[] ans=strs[i].toCharArray();
+         Arrays.sort(ans);
+         String sort=new String(ans);
+
+         if(!digit.containsKey(sort)){
+            digit.put(sort,new ArrayList<>());
          }
+         digit.get(sort).add(strs[i]);
       }
-      return true;
+      List<List<String>> result=new ArrayList<>();
+      for(Map.Entry<String,List<String>> enter:digit.entrySet()){
+         result.add(enter.getValue());
+      }
+      return result;
    }
    public static void main(String[] args){
-      System.out.print("Enter string 1:- ");
+      System.out.print("Enter string size:- ");
       Scanner sc=new Scanner(System.in);
-      String s=sc.nextLine();
-
-      System.out.print("Enter string 2:- ");
-      String t=sc.nextLine();
-
-      System.out.print(Anagram(s,t));
+      int n=sc.nextInt();
+      sc.nextLine();
+      String[] str=new String[n];
+      System.out.print("Enter strings:- ");
+      for(int i=0;i<n;i++){
+         str[i]=sc.nextLine();
+      }
+      List<List<String>> result=groupAnagrams(str);
+      System.out.print(result);
    }
 }
