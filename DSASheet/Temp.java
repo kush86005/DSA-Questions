91674f96e3f092b231775208755a82351b3621ac
kush <kush86005@gmail.com>
2025-08-21 23:49:50 +0530

    Approach 2

--- DSASheet/Temp.java
+++ DSASheet/Temp.java
@@ -2,24 +2,24 @@ package DSASheet;
 import java.util.*;
 public class Temp {
    public static boolean Anagram(String s,String t) {
+      HashMap<Character,Integer> digit=new HashMap<>();
       int n=s.length();
       int q=t.length();
       if(n!=q) return false;
-      HashMap<Character,Integer> digit=new HashMap<>();
       for(int i=0;i<n;i++){
          char c=s.charAt(i);
          digit.put(c,digit.getOrDefault(c,0)+1);
       }
-      HashMap<Character,Integer> answer=new HashMap<>();
-      for(int i=0;i<n;i++){
+      for(int i=0;i<q;i++){
          char c=t.charAt(i);
-         answer.put(c,answer.getOrDefault(c,0)+1);
+         if(!digit.containsKey(c)||digit.get(c)==0){
+            return false;
+         }
+         if(digit.containsKey(c)){
+            digit.put(c,digit.get(c)-1);
+         }
       }
-      if(digit.equals(answer)){
-         return true;
-      }else {
-         return false;
-      }
+      return true;
    }
    public static void main(String[] args){
       System.out.print("Enter string 1:- ");
