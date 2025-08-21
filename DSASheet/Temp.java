984c16fb8d8ddc499c498261f39721d1a52ae241
kush <kush86005@gmail.com>
2025-08-21 23:37:45 +0530

    Anagram(Approach 1)

--- DSASheet/Temp.java
+++ DSASheet/Temp.java
@@ -1,29 +1,34 @@
 package DSASheet;
 import java.util.*;
 public class Temp {
-   public static ArrayList<Integer> Dup(int[] arr) {
-      HashMap<Integer,Integer> digit=new HashMap<>();
-      int n=arr.length;
-      ArrayList<Integer> result=new ArrayList<>();
+   public static boolean Anagram(String s,String t) {
+      int n=s.length();
+      int q=t.length();
+      if(n!=q) return false;
+      HashMap<Character,Integer> digit=new HashMap<>();
       for(int i=0;i<n;i++){
-         digit.put(arr[i],digit.getOrDefault(arr[i],0)+1);
-
-         if(digit.get(arr[i])>1){
-            result.add(arr[i]);
-         }
+         char c=s.charAt(i);
+         digit.put(c,digit.getOrDefault(c,0)+1);
       }
-      return result;
+      HashMap<Character,Integer> answer=new HashMap<>();
+      for(int i=0;i<n;i++){
+         char c=t.charAt(i);
+         answer.put(c,answer.getOrDefault(c,0)+1);
+      }
+      if(digit.equals(answer)){
+         return true;
+      }else {
+         return false;
+      }
    }
    public static void main(String[] args){
-      System.out.print("Enter size:- ");
+      System.out.print("Enter string 1:- ");
       Scanner sc=new Scanner(System.in);
-      int n=sc.nextInt();
-      int[] arr=new int[n];
-      System.out.print("Enter elements:- ");
-      for(int i=0;i<n;i++){
-         arr[i]=sc.nextInt();
-      }
-      ArrayList<Integer> result=Dup(arr);
-         System.out.print(result);
+      String s=sc.nextLine();
+
+      System.out.print("Enter string 2:- ");
+      String t=sc.nextLine();
+
+      System.out.print(Anagram(s,t));
    }
 }
