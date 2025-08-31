52a997388eae08f580c7ae8dbdf4bf70ce8c9e85
kush <kush86005@gmail.com>
2025-08-31 19:41:06 +0530

    Majority elements

---
+++ DSAFinal/Question7.java
@@ -0,0 +1,29 @@
+package DSAFinal;
+import java.util.*;
+public class Question7 {
+    public static int majority(int[] arr){
+        int n=arr.length;
+        int ans=n/2;
+        HashMap<Integer,Integer> digit=new HashMap<>();
+        for(int count:arr){
+            digit.put(count,digit.getOrDefault(count,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> enter:digit.entrySet()){
+            if(enter.getValue()>ans){
+                return enter.getKey();
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.print("Enter elements:- ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print(majority(arr));
+    }
+}
