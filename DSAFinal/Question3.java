151c31fad75215033fb22bac13ef96ce4319c88f
kush <kush86005@gmail.com>
2025-08-31 17:41:37 +0530

    Contains duplicate(Leetcode)

---
+++ DSAFinal/Question3.java
@@ -0,0 +1,26 @@
+package DSAFinal;
+import java.util.*;
+public class Question3 {
+    public static boolean dup(int[] arr){
+        int n=arr.length;
+        HashMap<Integer,Integer> digit=new HashMap<>();
+        for(int count:arr){
+            digit.put(count,digit.getOrDefault(count,0)+1);
+            if(digit.get(count)>1){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.print("Enter element:- ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print(dup(arr));
+    }
+}
