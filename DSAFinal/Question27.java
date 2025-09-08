c417beb6576539df12b54b7d2f29ff11f407bd64
kush <kush86005@gmail.com>
2025-09-08 15:52:48 +0530

    3sum closest(GFG)(Brute force)

---
+++ DSAFinal/Question27.java
@@ -0,0 +1,36 @@
+package DSAFinal;
+import java.util.*;
+public class Question27 {
+    public static int closest(int[] arr,int target){
+        int n=arr.length;
+        int closest=Integer.MIN_VALUE;
+        int maxDiff=Integer.MAX_VALUE;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    int count=arr[i]+arr[j]+arr[k];
+                    int diff=Math.abs(target-count);
+                    if(diff<maxDiff||(diff==maxDiff&&count>closest)){
+                        maxDiff=diff;
+                        closest=count;
+                    }
+                }
+            }
+        }
+        return closest;
+    }
+    public static void main(String[] args){
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int[] arr=new int[s];
+        System.out.print("Enter elements:- ");
+        for(int i=0;i<s;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter target:- ");
+        int target=sc.nextInt();
+        int ans=closest(arr,target);
+        System.out.print(ans);
+    }
+}
