f19d7ae348426257a25f89b14b56b4f339dfab93
kush <kush86005@gmail.com>
2025-09-09 15:24:07 +0530

    Majority Element

--- DSAFinal/Question007.java
+++ DSAFinal/Question007.java
@@ -1,6 +1,6 @@
 package DSAFinal;
 import java.util.*;
-public class Question07 {
+public class Question007 {
     public static int majority(int[] arr){
         int n=arr.length;
         int ans=n/2;
