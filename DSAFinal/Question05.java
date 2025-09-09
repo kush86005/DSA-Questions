99d64b9d2c126489457273ba2cb96396edec0dd6
kush <kush86005@gmail.com>
2025-09-09 14:37:49 +0530

    Valid Anagram(Leetcode Optimized)

--- DSAFinal/Question05.java
+++ DSAFinal/Question05.java
@@ -1,28 +1,50 @@
 package DSAFinal;
 import java.util.*;
+
 public class Question05 {
-    public static boolean Anagram(String s,String t){
-        int n=s.length();
-        int[] alpha=new int[26];
-        for(int i=0;i<n;i++){
-            alpha[s.charAt(i)-'a']++;
-            alpha[t.charAt(i)-'a']--;
+    public static boolean isAnagram(String s, String t) {
+        // Remove this if you want case-sensitive anagram check
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+
+        if (s.length() != t.length()) {
+            return false;
         }
-        for(int count:alpha){
-            if(count!=0){
+
+        int[] alpha = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetter(s.charAt(i)) && Character.isLetter(t.charAt(i))) {
+                alpha[s.charAt(i) - 'a']++;
+                alpha[t.charAt(i) - 'a']--;
+            } else {
+                // Optional: Handle non-alphabetic characters
+                return false;
+            }
+        }
+
+        for (int count : alpha) {
+            if (count != 0) {
                 return false;
             }
         }
         return true;
     }
-    public static void main(String[] args){
-        System.out.print("Enter string 1:- ");
-        Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
 
-        System.out.print("Enter string 2:- ");
-        String t=sc.nextLine();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print(Anagram(s,t));
+        System.out.print("Enter string 1: ");
+        String s = sc.nextLine();
+
+        System.out.print("Enter string 2: ");
+        String t = sc.nextLine();
+
+        if (isAnagram(s, t)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are NOT anagrams.");
+        }
+
+        sc.close();
     }
 }
