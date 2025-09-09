6f75484eaca5dd390a3c23838f1c7af39f23cdb9
kush <kush86005@gmail.com>
2025-09-09 14:31:08 +0530

    2Sum(GFG)

--- DSAFinal/Question02.java
+++ DSAFinal/Question02.java
@@ -1,6 +1,6 @@
 package DSAFinal;
 import java.util.*;
-public class Question2 {
+public class Question02 {
     public static int twoSum(int[] arr,int target) {
         int n = arr.length;
         HashMap<Integer, Integer> digit = new HashMap<>();
