dae0f4907c950ee2c9f94fc7e80ff435905ed841
kush <kush86005@gmail.com>
2025-09-09 14:32:09 +0530

    Contains Duplicate(Leetcode)

--- DSAFinal/Question03.java
+++ DSAFinal/Question03.java
@@ -1,6 +1,6 @@
 package DSAFinal;
 import java.util.*;
-public class Question3 {
+public class Question03 {
     public static boolean dup(int[] arr){
         int n=arr.length;
         HashMap<Integer,Integer> digit=new HashMap<>();
