28c453f2b3b23a0cbfa443af03e768dc9040aefe
kush <kush86005@gmail.com>
2025-09-19 17:55:35 +0530

    Custom Lined list 1

--- LinkedList/Question01.java
+++ LinkedList/Question01.java
@@ -1,55 +1,69 @@
 package LinkedList;
 import java.util.*;
+
 public class Question01 {
-    class Node{
+    static class Node {
         String data;
         Node next;
 
-        Node(data){
-            this.data=data;
-            this.next=null;
+        Node(String data) {
+            this.data = data;
+            this.next = null;
         }
     }
-    Node head=null;
 
-    public static void insertion(String data){
-        Node val=new Node(data);
+    static Node head = null;
 
-        if(head==null){
-            head=val;
-        }else{
-            Node temp=head;
-            while(temp.next!=null){
-                temp=temp.next;
+    // Insert method fixed: now correctly links the new node
+    public static void insertion(String data) {
+        Node val = new Node(data);
+
+        if (head == null) {
+            head = val;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
-            temp=val;
+            temp.next = val;  // Fix here: link new node to the last node
         }
     }
-    public static void printlist(){
-        if(head==null){
-            System.out.print("The list is empty");
+
+    // Print method improved for better output
+    public static void printlist() {
+        if (head == null) {
+            System.out.println("The list is empty");
             return;
-        }else{
-            Node val= head;
-            while(val!=null){
-                System.out.print(val.data+" ");
-                val=val.next;
-            }
-            System.out.print("null");
         }
+
+        Node val = head;
+        while (val != null) {
+            System.out.print(val.data);
+            if (val.next != null) {
+                System.out.print(" -> ");
+            }
+            val = val.next;
+        }
+        System.out.println(" -> null");
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
 
-        while(true){
-            String data=sc.nextLine();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-            if(data.equalsIgnoreCase("exit")){
+        System.out.println("Enter elements (type 'exit' to stop):");
+        while (true) {
+            String data = sc.nextLine();
+
+            if (data.equalsIgnoreCase("exit")) {
                 break;
             }
-            list.insert(data);
+
+            insertion(data); // Call insertion directly, no object called 'list'
         }
-        System.out.print("The list is:- ");
-        list.printList();
+
+        System.out.print("The list is: ");
+        printlist();
+
+        sc.close();
     }
 }
