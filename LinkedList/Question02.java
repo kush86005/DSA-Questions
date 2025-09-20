ecb4b1c0a604990a530d00a197ab11ef650e20af
kush <kush86005@gmail.com>
2025-09-20 13:14:46 +0530

    Custom Linked list 2

--- LinkedList/Question02.java
+++ LinkedList/Question02.java
@@ -1,51 +1,52 @@
 package LinkedList;
 import java.util.*;
 public class Question02 {
-    static class Node{
-        String data;
-        Node next;
+   public static class Node{
+       String data;
+       Node next;
 
-        Node(String data){
-            this.data=data;
-            this.next=null;
-        }
-    }
-    static Node head=null;
+       Node(String data){
+           this.data=data;
+           this.next=null;
+       }
+   }
+   static Node head=null;
 
-    public static void insert(String data){
-        Node val=new Node(data);
-        if(head==null){
-            head=val;
-        }else{
-            Node temp=head;
-            while(temp.next!=null){
-                temp=temp.next;
-            }
-            temp.next=val;
-        }
-    }
-    public static void print(){
-        if(head==null){
-            System.out.print("The list is empty");
-        }else{
-            Node val=head;
-            while(val!=null){
-                System.out.print(val.data+" ");
-                val=val.next;
-            }
-        }
-    }
-    public static void main(String[] args){
-        System.out.print("Enter number if nodes:- ");
-        Scanner sc=new Scanner(System.in);
-        int nodes=sc.nextInt();
-        sc.nextLine();
-
-        for(int i=0;i<nodes;i++){
-            String data=sc.nextLine();
-            insert(data);
-        }
-        System.out.print("Linked List:- ");
-        print();
-    }
+   public static void Insertion(String data){
+       Node val=new Node(data);
+       if(head==null){
+           head=val;
+       }else{
+           Node temp=head;
+           while(temp.next!=null){
+               temp=temp.next;
+           }
+           temp.next=val;
+       }
+   }
+   public static void Print(){
+       if(head==null){
+           System.out.print("The list is empty");
+           return;
+       }else{
+           Node temp=head;
+           while(temp!=null){
+               System.out.print(temp.data+" ");
+               temp=temp.next;
+           }
+       }
+   }
+   public static void main(String[] args){
+       System.out.print("Enter number of nodes:- ");
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       sc.nextLine();
+       System.out.print("Enter values:- ");
+       for(int i=0;i<n;i++){
+           String data=sc.nextLine();
+           Insertion(data);
+       }
+       System.out.print("The list is:- ");
+       Print();
+   }
 }
