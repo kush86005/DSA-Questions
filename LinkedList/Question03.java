8f7413698948d330ddf0a23e6eb8f40fd2c65e33
kush <kush86005@gmail.com>
2025-09-20 13:40:04 +0530

    Add elements at the beginning

---
+++ LinkedList/Question03.java
@@ -0,0 +1,56 @@
+package LinkedList;
+import java.util.*;
+
+public class Question02 {
+    public static class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node head = null;
+
+    public static void Insertion(String data) {
+        Node val = new Node(data);
+        if (head == null) {
+            head = val;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = val;
+        }
+    }
+
+    public static void Print() {
+        if (head == null) {
+            System.out.print("The list is empty");
+            return;
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter number of nodes:- ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter values:- ");
+        for (int i = 0; i < n; i++) {
+            String data = sc.nextLine();
+            Insertion(data);
+        }
+        System.out.print("The list is:- ");
+        Print();
+    }
+}
