8b322f531325398d74c112fd5da73c6c7901f03b
kush <kush86005@gmail.com>
2025-09-21 17:17:29 +0530

    Reverse Linked List

---
+++ DSAFinal/Question052.java
@@ -0,0 +1,62 @@
+package DSAFinal;
+import java.util.*;
+public class Question052 {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    static Node head=null;
+    public static void insert(int data){
+        Node val=new Node(data);
+        if(head==null){
+            head=val;
+        }else{
+            Node temp=head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            temp.next=val;
+        }
+    }
+    public static void reverse(){
+        Node prev=null;
+        Node curr=head;
+        while(curr!=null){
+            Node next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    }
+    public static void print(){
+        if(head==null){
+            System.out.print("The list is empty");
+            return;
+        }else{
+            Node temp=head;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+        }
+    }
+    public static void main(String[] args){
+        System.out.print("Enter number if nodes:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        System.out.print("Enter elements:- ");
+        for(int i=0;i<n;i++){
+            int data=sc.nextInt();
+            insert(data);
+        }
+        reverse();
+        System.out.print("The reversed list is:- ");
+        print();
+    }
+}
