804d41483d238c4fe544e0175d7d878a9905dafd
kush <kush86005@gmail.com>
2025-09-21 20:32:08 +0530

    Mid of Linked List

---
+++ DSAFinal/Question056.java
@@ -0,0 +1,20 @@
+package DSAFinal;
+import java.util.*;
+public class Question056 {
+   /* class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            // Traverse the list with slow moving one step and fast moving two steps
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            // slow will be at the middle node when the loop ends
+            return slow;
+        }
+    }
+*/
+}
