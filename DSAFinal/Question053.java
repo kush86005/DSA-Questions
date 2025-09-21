5281d6f2875c5c885d511e0f13c0971f957b48d7
kush <kush86005@gmail.com>
2025-09-21 18:24:11 +0530

    Merge 2 sorted List

---
+++ DSAFinal/Question053.java
@@ -0,0 +1,39 @@
+package DSAFinal;
+import java.util.*;
+public class Question053 {
+   /* class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if(list1==null)return list2;
+            if(list2==null)return list1;
+
+            ListNode head=null;
+            ListNode curr=null;
+
+            if(list1.val<=list2.val){
+                head=list1;
+                list1=list1.next;
+            }else{
+                head=list2;
+                list2=list2.next;
+            }
+            curr=head;
+            while(list1!=null&&list2!=null){
+                if(list1.val<=list2.val){
+                    curr.next=list1;
+                    list1=list1.next;
+                }else{
+                    curr.next=list2;
+                    list2=list2.next;
+                }
+                curr=curr.next;
+            }
+            if(list1!=null){
+                curr.next=list1;
+            }else{
+                curr.next=list2;
+            }
+            return head;
+        }
+    }
+   */
+}
