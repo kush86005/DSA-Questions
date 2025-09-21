e5a2062b07335169fc007849ae4b755402a5eb6d
kush <kush86005@gmail.com>
2025-09-21 18:54:21 +0530

    Linked List Cycle

---
+++ DSAFinal/Question054.java
@@ -0,0 +1,19 @@
+package DSAFinal;
+import java.util.*;
+public class Question054 {
+    /*public boolean hasCycle(ListNode head) {
+        // Fast exit if list is empty or has only one element
+        if (head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head.next;  // Start fast one step ahead
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return false;  // Fast pointer reached the end
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;  // Cycle detected
+    }*/
+}
