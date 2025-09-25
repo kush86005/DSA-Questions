d5a3bef0c87691cecab70ab233f0dd3f902c32b6
kush <kush86005@gmail.com>
2025-09-25 21:44:39 +0530

    Delete the middle node of linked list

---
+++ LinkedList/Question13.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class Question13 {
+
+  /*  public static Node deleteMiddle(Node head) {
+        Node slow=head;
+        Node fast=head;
+        if(head.next==null){
+            return null;
+        }
+        while(fast.next!=null&&fast.next.next!=null&&fast.next.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }   */
+}
