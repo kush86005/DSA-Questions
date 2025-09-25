51585728e52de6e0ba4b4056dbd899f75f477513
kush <kush86005@gmail.com>
2025-09-25 21:25:39 +0530

    Find intersection point between 2 linked list

---
+++ LinkedList/Question12.java
@@ -0,0 +1,21 @@
+package LinkedList;
+//Find intersection point between 2 different linked list
+public class Question12 {
+   /* public Node intersectPoint(Node head1, Node head2) {
+        Node a=head1;
+        Node b=head2;
+        while(a!=b){
+            if(a==null){
+                a=head2;
+            }else{
+                a=a.next;
+            }
+            if(b==null){
+                b=head1;
+            }else{
+                b=b.next;
+            }
+        }
+        return a;
+    }*/
+}
