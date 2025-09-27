e907a2d3fce1e5bc3f4f88366c96942aca153595
kush <kush86005@gmail.com>
2025-09-27 20:50:28 +0530

    Stack overflow & underflow(Theory)

---
+++ DoublyLinkedList/Stack.java
@@ -0,0 +1,12 @@
+package DoublyLinkedList;
+
+public class Stack {
+    /*Stack Underflow:- It occurs when you try to pop an item from an empty stack or try to peek at the top of an empty stack.
+    It's an error condition that happens when there are no elements to remove from the stack but an operation is still attempted.*/
+
+    /* Stack Overflow occurs when you try to push an element onto a stack that is already full.
+    In other words, if the stack's capacity is reached and an attempt is made to add more elements, this leads to an overflow condition.
+
+    In a fixed-size stack, such as one implemented with an array,
+    once the number of elements exceeds the size of the array, you can't add any more elements, resulting in stack overflow.*/
+}
