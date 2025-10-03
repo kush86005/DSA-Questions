73c8c46701027aa2803f011f1fb00d4b7a40df7a
kush <kush86005@gmail.com>
2025-10-03 19:14:43 +0530

    Max value in a BST

---
+++ Trees/Question06.java
@@ -0,0 +1,52 @@
+package Trees;
+import java.util.*;
+public class Question06 {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static Node root=null;
+    public static void insert(int data){
+        root=recursion(root,data);
+    }
+    private static Node recursion(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        if(data<root.data){
+            root.left=recursion(root.left,data);
+        }
+        if(data>root.data){
+            root.right=recursion(root.right,data);
+        }
+        return root;
+    }
+    public static int Max(Node root){
+        if(root==null){
+            return 0;
+        }
+        while(root.right!=null){
+            root=root.right;
+        }
+        return root.data;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter number of nodes:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print("Enter elements:- ");
+        for(int i=0;i<n;i++){
+            int val=sc.nextInt();
+            insert(val);
+        }
+        int ans=Max(root);
+        System.out.print(ans);
+    }
+ }
