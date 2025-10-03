1d825f7673bf95dd3b74a467a726d2cd17e38874
kush <kush86005@gmail.com>
2025-10-03 14:53:22 +0530

    Post order traversal

---
+++ Trees/Question04.java
@@ -0,0 +1,57 @@
+package Trees;
+import java.util.*;
+//Postorder traversal: Used to delete or free nodes safely by visiting children before their parent.
+//Left → Right → Root
+public class Question04 {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static Node root=null;
+    public static void insert(int data){
+        root=recursion(root,data);
+    }
+    private static Node recursion(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        if(data<root.data){
+            root.left=recursion(root.left,data);
+        }
+        if(data>root.data){
+            root.right=recursion(root.right,data);
+        }
+        return root;
+    }
+    public static void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter number of nodes:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print("Enter elements:- ");
+        for(int i=0;i<n;i++){
+            int val=sc.nextInt();
+            insert(val);
+        }
+        if(root==null){
+            System.out.print("The tree is empty");
+        }else{
+            System.out.print("Postorder traversal:- ");
+            postorder(root);
+        }
+    }
+}
