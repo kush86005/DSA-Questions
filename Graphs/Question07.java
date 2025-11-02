3aa6869718108510928a7ff52bfd759ababd3405
kush <kush86005@gmail.com>
2025-11-02 19:30:43 +0530

    Topological sort(Theory)

---
+++ Graphs/Question07.java
@@ -0,0 +1,17 @@
+package Graphs;
+
+public class Question07 {
+    /*Topological Sort is a way of arranging the vertices of a directed graph in a linear order such that
+    ➡️ for every directed edge u → v, vertex u comes before vertex v in the order.*/
+
+    /*👉 Topological Sort can be applied only on a Directed Acyclic Graph (DAG)
+      because, If there is a cycle, then there’s no valid way to arrange the nodes —
+      you’d keep going in circles.*/
+
+    /*Suppose you have tasks with dependencies: A → B → C
+                                                A → D
+
+      Then a valid topological order could be: A, B, D, C
+     */
+
+}
