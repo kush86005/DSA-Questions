dfef442bbd6f3e45ea15a86c90bb4e1d1682ab6f
kush <kush86005@gmail.com>
2025-11-13 02:30:05 +0530

    Longest Increasing Subsequence

---
+++ DynamicProgramming/Question17.java
@@ -0,0 +1,34 @@
+package DynamicProgramming;
+import java.util.*;
+public class Question17 {
+    public static int longestIncreasingSubsequence(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int maxLength = 0;
+        for (int length : dp) {
+            maxLength = Math.max(maxLength, length);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print("Enter elements:- ");
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int ans=longestIncreasingSubsequence(arr);
+        System.out.print(ans);
+    }
+}
