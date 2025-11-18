40ab9c070c7af838200b66bd51107ec66ed626fb
kush <kush86005@gmail.com>
2025-11-18 22:58:51 +0530

    Contains Duplicate(GFG)

--- MasterSheet/Question004.java
+++ MasterSheet/Question004.java
@@ -1,6 +1,6 @@
 package MasterSheet;
 import java.util.*;
-public class Question04 {
+public class Question004 {
     public static ArrayList<Integer> duplicate(int[] arr){
         HashMap<Integer,Integer> map=new HashMap<>();
         ArrayList<Integer> ans=new ArrayList<>();
