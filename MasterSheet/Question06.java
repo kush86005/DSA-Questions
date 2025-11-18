e936c9ec9e9d0974bd5cc708543b774f25367c16
kush <kush86005@gmail.com>
2025-11-18 12:01:32 +0530

    Group Anagram

---
+++ MasterSheet/Question06.java
@@ -0,0 +1,22 @@
+package MasterSheet;
+import java.util.*;
+public class Question06 {
+    public static List<List<String>> anagram(String[] str){
+        HashMap<String,ArrayList<String>> map=new HashMap<>();
+        for(int i=0;i<str.length;i++){
+            char[] ch=str[i].toCharArray();
+            Arrays.sort(ch);
+            String s=new String(ch);
+
+            map.putIfAbsent(s, new ArrayList<>());
+
+            map.get(s).add(str[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String[] args) {
+        String[] str=new String[]{"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> ans=anagram(str);
+        System.out.print(ans);
+    }
+}
