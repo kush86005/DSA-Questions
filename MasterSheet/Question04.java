9d3c4c69e1d5491c7ef821c306576cd956bc6675
kush <kush86005@gmail.com>
2025-11-18 11:25:53 +0530

    Contains Duplicate(GFG)

---
+++ MasterSheet/Question04.java
@@ -0,0 +1,21 @@
+package MasterSheet;
+import java.util.*;
+public class Question04 {
+    public static ArrayList<Integer> duplicate(int[] arr){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int nums:arr){
+            map.put(nums,map.getOrDefault(nums,0)+1);
+
+            if(map.get(nums)>1){
+                ans.add(nums);
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr=new int[]{2, 3, 1, 2, 3};
+        ArrayList<Integer> ans=duplicate(arr);
+        System.out.print(ans);
+    }
+}
