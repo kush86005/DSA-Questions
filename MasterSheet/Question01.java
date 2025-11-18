efbefa551f6cf149367642350b4adb4d41969500
kush <kush86005@gmail.com>
2025-11-18 11:04:02 +0530

    Two Sum(LC)

---
+++ MasterSheet/Question01.java
@@ -0,0 +1,36 @@
+package MasterSheet;
+import java.util.*;
+public class Question01 {
+    public static int[] twoSum(int[] arr,int target){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],i);
+        }
+        int[] ans=new int[2];
+        for(int i=0;i<arr.length;i++){
+            int val=target-arr[i];
+            if(map.containsKey(val)){
+                ans[0]=i;
+                ans[1]=map.get(val);
+                break;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter size:- ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.print("Enter elements:- ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter target:- ");
+        int target=sc.nextInt();
+        int[] ans=twoSum(arr,target);
+        for(int i=0;i<2;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
