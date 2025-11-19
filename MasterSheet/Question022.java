d2182ef95494bb75825e76287ef58c56edca08a7
kush <kush86005@gmail.com>
2025-11-19 15:34:19 +0530

    Valid Parenthesis

--- MasterSheet/Question022.java
+++ MasterSheet/Question022.java
@@ -3,22 +3,22 @@ import java.util.*;
 public class Question022 {
     public static boolean valid(String s){
         Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
+        int n=s.length();
+        for(int i=0;i<n;i++){
             char ch=s.charAt(i);
             if(ch=='['||ch=='{'||ch=='('){
                 stack.push(ch);
-            }else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[' ||
-                    ch == '}' && !stack.isEmpty() && stack.peek() == '{' ||
-                    ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+            }else if(ch==']'&&stack.peek()=='['&&!stack.isEmpty()
+                    ||ch=='}'&&stack.peek()=='{'&&!stack.isEmpty()
+                    ||ch==')'&&stack.peek()=='('&&!stack.isEmpty()){
                 stack.pop();
-            } else {
-                return false;
             }
         }
         return stack.isEmpty();
     }
+
     public static void main(String[] args) {
-        String s="()[]{}";
+        String s="[()()]{}";
         System.out.print(valid(s));
     }
 }
