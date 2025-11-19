f5c7d409b22c9d53e1d9e46c90368a44c42b9f37
kush <kush86005@gmail.com>
2025-11-19 23:52:07 +0530

    Evaluate reverse polish notation

---
+++ MasterSheet/Question024.java
@@ -0,0 +1,37 @@
+package MasterSheet;
+import java.util.*;
+public class Question024 {
+    public static int reverse(String[] token){
+        Stack<Integer> stack=new Stack<>();
+        for(String count:token){
+            if(count.equals("+")||count.equals("-")||count.equals("*")||count.equals("/")){
+                int a=stack.pop();
+                int b=stack.pop();
+                int result=0;
+                switch(count){
+                    case"+":
+                    result=a+b;
+                    break;
+                    case"-":
+                    result=a-b;
+                    break;
+                    case"*":
+                    result=a*b;
+                    break;
+                    case"/":
+                    result=a/b;
+                    break;
+                }
+                stack.push(result);
+            }else{
+                stack.push(Integer.parseInt(count));
+            }
+        }
+        return stack.pop();
+    }
+    public static void main(String[] args) {
+        String[] token=new String[]{"2","1","+","3","*"};
+        int ans=reverse(token);
+        System.out.print(ans);
+    }
+}
