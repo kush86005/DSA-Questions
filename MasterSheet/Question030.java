31d96b9c42ff0b0f5aed846f8b5a39391af221ee
kush <kush86005@gmail.com>
2025-12-09 12:35:34 +0530

    Search in rotated sorted array

---
+++ MasterSheet/Question030.java
@@ -0,0 +1,34 @@
+package MasterSheet;
+import java.util.*;
+public class Question030 {
+    public static int rotated(int[] arr,int target){
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[i]<=arr[mid]){
+                if(arr[i]<=target&&target<arr[mid]){
+                    j=mid-1;
+                }else{
+                    i=mid+1;
+                }
+            }else{
+                if(arr[mid]<target&&target<=arr[j]){
+                    i=mid+1;
+                }else{
+                    j=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums=new int[]{4,5,6,7,0,1,2};
+        int target=0;
+        System.out.print(rotated(nums,target));
+    }
+}
