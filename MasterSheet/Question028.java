d0dfae0ee68adf64b8e2dcf47a8b1bc5cf0f4d68
kush <kush86005@gmail.com>
2025-12-09 10:42:59 +0530

    Search in a sorted matrix

---
+++ MasterSheet/Question028.java
@@ -0,0 +1,38 @@
+package MasterSheet;
+import java.util.*;
+public class Question028 {
+    public static boolean gridsearch(int[][] mat,int target){
+        int row=mat.length;
+        int column=mat[0].length;
+        if(mat==null||row==0||column==0){
+            return false;
+        }
+        int i=0;
+        int j=row*column-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            int current=mat[mid/column][mid%column];
+            if(current>target){
+                j=mid-1;
+            }else if(current<target){
+                i=mid+1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+
+        int target1 = 16;
+        int target2 = 13;
+
+        System.out.println("Searching for " + target1 + ": " + gridsearch(matrix, target1));
+        System.out.println("Searching for " + target2 + ": " + gridsearch(matrix, target2));
+    }
+}
