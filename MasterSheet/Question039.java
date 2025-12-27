af60c93b0b0554c59b22239711307ad5ac7bb24d
kush <kush86005@gmail.com>
2025-12-28 01:21:59 +0530

    Mid element in Linked List

---
+++ MasterSheet/Question039.java
@@ -0,0 +1,47 @@
+package MasterSheet;
+import java.util.*;
+public class Question039 {
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    Node head=null;
+    Node tail=null;
+
+    public void insert(int data){
+        Node val=new Node(data);
+        if(head==null){
+            head=val;
+            tail=val;
+        }else{
+            tail.next=val;
+            tail=val;
+        }
+    }
+    public static int mid(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow.data;
+    }
+    public static void main(String[] args) {
+        Question039 list = new Question039();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+
+        int ans=mid(list.head);
+        System.out.print(ans);
+    }
+}
