a0cc92455092063509eda547008e1ea91a72c6be
kush <kush86005@gmail.com>
2026-01-16 16:20:19 +0530

    Contains Duplicate

--- MasterSheet/Question002.java
+++ MasterSheet/Question002.java
@@ -1,30 +1,19 @@
 package MasterSheet;
 import java.util.*;
 public class Question002 {
-    public static int twoSum(int[] arr,int target){
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            int val=target-arr[i];
-            if(map.containsKey(val)){
-                count+=map.get(val);
+    public static boolean duplicate(int[] arr){
+        HashSet<Integer> set=new HashSet<>();
+        for(int nums:arr){
+            if(set.contains(nums)){
+                return true;
             }
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            set.add(nums);
         }
-        return count;
+        return false;
     }
     public static void main(String[] args) {
-        System.out.print("Enter size:- ");
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.print("Enter elements:- ");
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        System.out.print("Enter target:- ");
-        int target=sc.nextInt();
-        int ans=twoSum(arr,target);
+        int[] arr=new int[]{1,1,1,3,3,4,3,2,4,2};
+        boolean ans=duplicate(arr);
         System.out.print(ans);
     }
 }
