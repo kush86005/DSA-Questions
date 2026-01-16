f70df185f75dfbd57e6d93a6849deb4046c31313
kush <kush86005@gmail.com>
2026-01-16 16:23:57 +0530

    Group Anagram

--- MasterSheet/Question004.java
+++ MasterSheet/Question004.java
@@ -1,6 +1,6 @@
 package MasterSheet;
 import java.util.*;
-public class Question006 {
+public class Question004 {
     public static List<List<String>> anagram(String[] str){
         HashMap<String,ArrayList<String>> map=new HashMap<>();
         for(int i=0;i<str.length;i++){
