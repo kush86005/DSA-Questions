73d2645534b9c3c2c9e5e5ea8c0d5d7943456af4
Kush <kush86005@gmail.com>
2026-04-13 08:26:40 +0530

    Top K Frequent Elements

---
+++ MasterSheet/Question062.java
@@ -0,0 +1,30 @@
+package MasterSheet;
+import java.util.*;
+public class Question062 {
+    public static List<Integer> TopKFreq(int[] arr, int k){
+        int n=arr.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1;
+        }
+        PriorityQueue<int[]> minHeap=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            minHeap.offer(new int[]{map.entry.getKey(),entry.getValue});
+            if(minHeap.size<k){
+                minHeap.poll();
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        while(!minHeap.isEmpty()){
+            ans.add(minHeap.poll()[0]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,2,2,3};
+        int k = 2;
+
+        List<Integer> res = topKFreq(arr, k);
+        System.out.println(res);
+    }
+}
